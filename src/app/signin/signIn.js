c796aea709b43b4ec4012ceb21dcf8bc1af2a5bd
leejjeonghui <gdjh0513@naver.com>
2024-07-16 20:19:33 +0900

    회원가입 + 로그인

--- src/app/signin/signIn.js
+++ src/app/signin/signIn.js
@@ -14,16 +14,18 @@ const app = initializeApp(firebaseConfig);
 const auth = getAuth();
 
 
-const Signin = (signInEmail, signInPassword) => {
-    return signInWithEmailAndPassword(auth, signInEmail, signInPassword)
-        .then((userCredential) => {
-            const user = userCredential.user;
-            console.log(user);
-        })
-        .catch((error) => {
-            console.error('로그인 실패', error.message);
-            throw new Error(error.message); 
-        });
-};
+const Signin=(signInEmail, signInPassword) => {
+    signInWithEmailAndPassword(auth, signInEmail, signInPassword)
+.then((userCredential) => {
+  // Signed in 
+  const user = userCredential.user;
+  console.log(user);
+  // ...
+})
+.catch((error) => {
+  const errorCode = error.code;
+  const errorMessage = error.message;
+})};
 
 export default Signin;
+
